cdfea340d06091888699498c887cdef97f64f505
sristhikumari63-boop <sristhikumari63@gmail.com>
2026-07-17 10:19:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: '#F4F4F6', margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
+      </head>
+      <body style={{ backgroundColor: '#F8F9FA', margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
         {children}
       </body>
     </html>
